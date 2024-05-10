8292942430ac554bbf54cd19625ec6258193d40f
Ali Rahimi <ali.rahimi@gandom.co>
2024-05-10 15:10:36 +0330

    music player added

--- app/src/main/java/com/alirahimi/androidcourse/MainActivity.kt
+++ app/src/main/java/com/alirahimi/androidcourse/MainActivity.kt
@@ -1,39 +1,42 @@
 package com.alirahimi.androidcourse
 
-import android.content.Intent
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.Room
-import com.alirahimi.androidcourse.adapter.UsersAdapter
-import com.alirahimi.androidcourse.data.Constants
 import com.alirahimi.androidcourse.databinding.ActivityMainBinding
-import com.alirahimi.androidcourse.db.UserDatabase
+import com.google.android.material.slider.Slider
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     //region properties
     private lateinit var binding: ActivityMainBinding
-    private val userDb: UserDatabase by lazy {
-        Room.databaseBuilder(this, UserDatabase::class.java, Constants.USER_DATABASE)
-            .allowMainThreadQueries()
-            .fallbackToDestructiveMigration()
-            .build()
-    }
-    private val usersAdapter by lazy { UsersAdapter() }
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var timer: Timer
+    private lateinit var coverImageAnimator: ObjectAnimator
+    private var isPlaying = true
+    private var isUserChanging = false
+    private var isMute = false
     //endregion
 
     //region lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initialBinding()
+        prepareMusic()
         configListeners()
+        configSlider()
+        setupCoverImageRotation()
     }
 
-    override fun onResume() {
-        super.onResume()
-        checkItems()
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+        mediaPlayer.release()
     }
     //endregion
 
@@ -43,34 +46,148 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    private fun configListeners() {
-        binding.addUserButton.setOnClickListener {
-            startActivity(Intent(this@MainActivity, AddUserActivity::class.java))
-        }
-    }
+    private fun prepareMusic() {
 
-    private fun checkItems() {
-        binding.apply {
-            if (userDb.userDao().getAllUsers().isNotEmpty()) {
-                usersList.visibility = View.VISIBLE
-                emptyText.visibility = View.GONE
+        mediaPlayer = MediaPlayer.create(this, R.raw.ranj8)
 
-                usersAdapter.differ.submitList(userDb.userDao().getAllUsers())
-                setupRecyclerView()
+        mediaPlayer.start()
+        isPlaying = true
 
-            } else {
-                usersList.visibility = View.GONE
-                emptyText.visibility = View.VISIBLE
+        binding.buttonPlayPause.setImageResource(R.drawable.ic_pause)
+
+        binding.sliderMain.valueTo = mediaPlayer.duration.toFloat()
+
+        binding.textRight.text = convertMillisToString(mediaPlayer.duration.toLong())
+
+        timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    if (!isUserChanging) {
+                        binding.sliderMain.value = mediaPlayer.currentPosition.toFloat()
+                    }
+                }
             }
+        }, 1000, 1000)
+    }
+
+    private fun configListeners() {
+        binding.buttonPlayPause.setOnClickListener { configureMusic() }
+        binding.buttonGoBefore.setOnClickListener { goBeforeMusic() }
+        binding.buttonGoAfter.setOnClickListener { goAfterMusic() }
+        binding.buttonVolumeOnOff.setOnClickListener { configureVolume() }
+        binding.sliderMain.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+
+            @SuppressLint("RestrictedApi")
+            override fun onStartTrackingTouch(slider: Slider) {
+            }
+
+            @SuppressLint("RestrictedApi")
+            override fun onStopTrackingTouch(slider: Slider) {
+                mediaPlayer.seekTo(slider.value.toInt())
+            }
+
+        })
+
+        //TODO apply
+    }
+
+    private fun configSlider() {
+        binding.sliderMain.addOnChangeListener { _, value, fromUser ->
+            binding.textLeft.text = convertMillisToString(value.toLong())
+            isUserChanging = fromUser
         }
     }
 
-    private fun setupRecyclerView() {
-        binding.usersList.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = usersAdapter
+    //    @SuppressLint("InlinedApi")
+//    private fun configureVolume() {
+//
+//        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+//
+//        isMute = if (isMute) {
+//            audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_SHOW_UI)
+//            binding.buttonVolumeOnOff.setImageResource(R.drawable.ic_volume_on)
+//            false
+//        } else {
+//            audioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI)
+//            binding.buttonVolumeOnOff.setImageResource(R.drawable.ic_volume_off)
+//            true
+//        }
+//    }
+
+    @SuppressLint("InlinedApi")
+    private fun configureVolume() {
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+
+        isMute = !isMute
+        audioManager.adjustVolume(
+            if (isMute) AudioManager.ADJUST_MUTE else AudioManager.ADJUST_UNMUTE,
+            AudioManager.FLAG_SHOW_UI
+        )
+        binding.buttonVolumeOnOff.setImageResource(
+            if (isMute) R.drawable.ic_volume_off else R.drawable.ic_volume_on
+        )
+    }
+
+    //    private fun goAfterMusic() {
+//        val now = mediaPlayer.currentPosition
+//        val newValue = now + 15000
+//        mediaPlayer.seekTo(newValue)
+//    }
+    private fun goAfterMusic() = mediaPlayer.seekTo(mediaPlayer.currentPosition + 15000)
+
+    private fun goBeforeMusic() = mediaPlayer.seekTo(mediaPlayer.currentPosition - 15000)
+
+
+//    private fun goBeforeMusic() {
+//        val now = mediaPlayer.currentPosition
+//        val newValue = now - 15000
+//        mediaPlayer.seekTo(newValue)
+//    }
+
+    //    private fun configureMusic() {
+//        isPlaying = if (isPlaying) {
+//            mediaPlayer.pause()
+//            binding.buttonPlayPause.setImageResource(R.drawable.ic_play)
+//            binding.textTop.text = getString(R.string.now_pause)
+//            false
+//        } else {
+//            mediaPlayer.start()
+//            binding.buttonPlayPause.setImageResource(R.drawable.ic_pause)
+//            binding.textTop.text = getString(R.string.now_playing)
+//            true
+//        }
+//    }
+    private fun configureMusic() {
+        isPlaying = !isPlaying
+        if (isPlaying) {
+            mediaPlayer.start()
+            binding.buttonPlayPause.setImageResource(R.drawable.ic_pause)
+            binding.textTop.text = getString(R.string.now_playing)
+            //coverImageAnimator.resume()
+        } else {
+            mediaPlayer.pause()
+            binding.buttonPlayPause.setImageResource(R.drawable.ic_play)
+            binding.textTop.text = getString(R.string.now_pause)
+            //coverImageAnimator.pause()
         }
     }
+
+    private fun convertMillisToString(duration: Long): String {
+        val seconds = (duration / 1000) % 60
+        val minutes = (duration / (1000 * 60)) % 60
+        return "%02d:%02d".format(minutes, seconds)
+    }
+
+
+    private fun setupCoverImageRotation() {
+        coverImageAnimator = ObjectAnimator.ofFloat(binding.coverMusic, "rotation", 0f, 360f)
+        coverImageAnimator.duration = 20000
+        coverImageAnimator.repeatCount = ObjectAnimator.INFINITE
+        coverImageAnimator.interpolator = LinearInterpolator()
+        coverImageAnimator.start()
+    }
+
     //endregion
-}
 
+}
--- app/src/main/java/com/alirahimi/androidcourse/data/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.alirahimi.androidcourse.data
-
-object Constants {
-    const val USER_TABLE = "user_table"
-    const val USER_TABLE_USER_NAME_COLUMN = "user_name"
-    const val USER_DATABASE = "user_database"
-    const val BUNDLE_USER_ID = "bundle_user_id"
-}
