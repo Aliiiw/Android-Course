01e1af5791112793dcee2c7eea4f2b347d621e61
Ali Rahimi <ali.rahimi@gandom.co>
2024-06-07 20:04:59 +0330

    flow files

--- app/src/main/java/com/alirahimi/androidcourse/MainActivity.kt
+++ app/src/main/java/com/alirahimi/androidcourse/MainActivity.kt
@@ -1,90 +1,199 @@
 package com.alirahimi.androidcourse
 
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.alirahimi.androidcourse.databinding.ActivityMainBinding
-import com.alirahimi.androidcourse.feature.post.presentation.ui.adapter.PostAdapter
-import com.alirahimi.androidcourse.feature.post.presentation.viewmodel.PostViewModel
-import com.alirahimi.androidcourse.feature.post.presentation.viewmodel.PostViewModelFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 class MainActivity : AppCompatActivity() {
 
-    //region properties
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: PostViewModel
-    //endregion
-
-    //region lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initialBinding()
-        initialViewModel()
-        configViewModel()
-        callApi()
+        initialFlow()
     }
-    //endregion
 
-    //region methods
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun initialFlow() {
+        //TODO 1. create flow
+        //TODO 2. emit flow
+        //TODO 3. cold / collect
+        //TODO 4. stream
+
+        runBlocking {
+            flow {
+                delay(2000)
+                emit(2)
+            }.collect {
+                Log.e("2323", it.toString())
+            }
+        }
+
+        /////////////////////////////////////////////////////
+
+//        val myFlow = flow {
+//            delay(2000)
+//            emit(5)
+//        }
+//
+//        runBlocking {
+//            myFlow.collect {
+//                Log.e("2323", it.toString())
+//            }
+//        }
+
+        /////////////////////////////////////////////////////
+
+        val myFlow = flow {
+            delay(1000)
+            emit(1)
+            delay(1000)
+            emit(2)
+            delay(1000)
+            emit(3)
+            delay(1000)
+            emit(4)
+            delay(1000)
+            emit(5)
+        }
+        runBlocking {
+            myFlow.collect {
+                Log.e("2323", it.toString())
+            }
+        }
+
+        ///////////////////////////////////////////////////////
+
+        runBlocking {
+            power2().collect {
+                Log.e("2323", it.toString())
+            }
+        }
+
+        /////////////////////////////////////////////////////
+
+        //TODO 5. on collections
+        runBlocking {
+            sendNumbers2().collect {
+                Log.e("2323", it.toString())
+            }
+        }
+
+        /////////////////////////////////////////////////////
+
+        // TODO 6. cancellation / when coroutines cancel / not it self
+        val job = GlobalScope.launch {
+            power2().collect {
+                Log.e("2323", it.toString())
+            }
+        }
+
+        Thread.sleep(5000)
+        job.cancel()
+
+        runBlocking {
+            withTimeout(5000) {
+                power2().collect {
+                    Log.e("2323", it.toString())
+                }
+            }
+        }
+
+        /////////////////////////////////////////////////////
+
+        //TODO 7. collect / collectLatest ->
+        // if it takes more that usual and new emit come cancel olds
+        runBlocking {
+            power2().collectLatest {
+                //delay(90)
+                delay(100)
+                Log.e("2323", it.toString())
+            }
+        }
+
+
+        /////////////////////////////////////////////////////
+
+        //TODO 8. filter
+        GlobalScope.launch {
+            myTestFlow().filter {
+                    it.mod(2) == 0
+                }.collect {
+                    Log.e("2323", it.toString())
+                }
+        }
+
+        ///////////////////////////////////////////////////////
+
+        //TODO 8. map
+        GlobalScope.launch {
+            myTestFlow().filter {
+                    it.mod(2) == 0
+                }.map {
+                    "number is: $it"
+                }.collect {
+                    Log.e("2323", it.toString())
+                }
+        }
+
+        /////////////////////////////////////////////////////
+
+        //TODO 8. transform
+        GlobalScope.launch {
+            myTestFlow().filter {
+                    it.mod(2) == 0
+                }.map {
+                    it * 10
+                }.transform {
+                    emit(it)
+                    emit("new emit from transform: $it")
+                }.collect {
+                    Log.e("2323", it.toString())
+                }
+        }
+
+        /////////////////////////////////////////////////////
+
+        //TODO 8. onEach
+        //TODO 9. take
+
+        /////////////////////////////////////////////////////
+
+        //TODO 10. toList
+        GlobalScope.launch {
+            myTestFlow().filter {
+                    it.mod(2) == 0
+                }.toList().forEach {
+                    Log.e("2323", "item: $it")
+                }
+        }
+    }
+
+
+    private fun myTestFlow() = flow {
+        for (i in 1..100) {
+            delay(100)
+            emit(i)
+        }
+    }
+
+    private fun sendNumbers() = listOf(1, 2, 3, 4, 5).asFlow()
+    private fun sendNumbers1() = flowOf(1, 2, 3, 4, 5)
+    private fun sendNumbers2() = flowOf(1, "hi", true, 4.5, 5L, -1)
+
+    private fun power2(): Flow<Int> = flow {
+        for (i in 1..200) {
+            delay(100)
+            emit(i * i)
+        }
+    }
+
     private fun initialBinding() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-
-    private fun initialViewModel() {
-        viewModel = ViewModelProvider(this, PostViewModelFactory())[PostViewModel::class.java]
-    }
-
-    private fun configViewModel() {
-        viewModel.posts.observe(this) { posts ->
-            val list = posts.take(7)
-            val adapter = PostAdapter(list)
-
-            binding.postRecyclerView.adapter = adapter
-            binding.postRecyclerView.layoutManager =
-                LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-            binding.postRecyclerView.addItemDecoration(
-                DividerItemDecoration(
-                    this@MainActivity,
-                    LinearLayoutManager.VERTICAL
-                )
-            )
-
-            binding.postRecyclerView.itemAnimator = DefaultItemAnimator()
-
-
-        }
-
-        viewModel.loadingState.observe(this) {
-            if (it) {
-                ////
-            } else {
-
-            }
-        }
-    }
-
-    private fun callApi() {
-
-        CoroutineScope(Dispatchers.Main).launch {
-            binding.constraintAll.visibility = View.INVISIBLE
-            binding.linearLoading.visibility = View.VISIBLE
-            delay(2000)
-            viewModel.getAllPost()
-            binding.constraintAll.visibility = View.VISIBLE
-            binding.linearLoading.visibility = View.GONE
-        }
-
-    }
-    //endregion
 }
 
